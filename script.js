85de13b0ed485c42b1e8c1e0b41380865b355634
Manh Phan <dphan8@uic.edu>
2024-02-07 01:16:42 -0600

    finish the hovering effect

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
 console.log("hello world");
 
+const hoverDelay = 5000;
+let size;
+
 const bigDiv = document.querySelector(".BigDiv");
 
-// create a 16 by 16 grids
-for (let i = 0; i < 16; i++) {
-    // create a row
-    const rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "rowDiv");
+// function for creating a line effect for hovering
+function putColor() {
+    this.style.backgroundColor = "black";
+}
 
-    // append row to BigDiv
-    bigDiv.appendChild(rowDiv);
+function outColor() {
+    let current = this;
+    setTimeout(function() {
+        current.style.backgroundColor = "white";
+    }, hoverDelay);
+}
 
-    let rowLength = rowDiv.offsetWidth;
-    console.log(`length: ${rowLength}`);
 
-    // put 16 elements in that row
-    for (let j = 0; j < 16; j++) {
-        let smallDiv = document.createElement("div");
-        smallDiv.setAttribute("class", "smallDiv");
-        // smallDiv.style.height = `${rowLength / 16}px`;
-        rowDiv.appendChild(smallDiv);
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
+
+// button
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    size = parseInt(prompt("Please enter the size of the grids"));
+
+    removeAllChildNodes(bigDiv);
+
+    // create a size by size grids
+    for (let i = 0; i < size; i++) {
+        // create a row
+        const rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class", "rowDiv");
+
+        // append row to BigDiv
+        bigDiv.appendChild(rowDiv);
+
+        let rowLength = rowDiv.offsetWidth;
+        console.log(`length: ${rowLength}`);
+
+        // put 16 elements in that row
+        for (let j = 0; j < 16; j++) {
+            let smallDiv = document.createElement("div");
+            smallDiv.setAttribute("class", "smallDiv");
+            smallDiv.addEventListener("mouseover", putColor);
+            smallDiv.addEventListener("mouseout", outColor)
+            rowDiv.appendChild(smallDiv);
+        }
+    }
+})
+
